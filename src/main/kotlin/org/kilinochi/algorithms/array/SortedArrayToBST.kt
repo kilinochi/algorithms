2cdec23055ca4d9007dbbe4f6f7b36bb8dfa32e3
a-sham <Ashamenov@alfabank.ru>
2023-09-02 17:24:45 +0600

    some changes

---
+++ src/main/kotlin/org/kilinochi/algorithms/array/SortedArrayToBST.kt
@@ -0,0 +1,39 @@
+package org.kilinochi.algorithms.array
+
+object SortedArrayToBST {
+
+    // *
+    //  Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced BST
+    //
+    //  Example 1:
+    //  Input: nums = [-10,-3,0,5,9]
+    //  Output: [0,-3,9,-10,null,5]
+    //  Explanation: [0,-10,5,null,-3,null,9] is also accepted
+    //
+    //  Example 2:
+    //  Input: nums = [1,3]
+    //  Output: [3,1]
+    //  Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+    // * //
+    fun sortedArrayToBST(sortedArray: IntArray): TreeNode? {
+        if (sortedArray.isEmpty()) return null
+
+        val mid = sortedArray.size / 2
+
+        val root = TreeNode(sortedArray[mid])
+        val leftArray = sortedArray.sliceArray(0..<mid)
+        val rightArray = sortedArray.sliceArray(mid + 1..<sortedArray.size)
+
+        root.left = sortedArrayToBST(leftArray)
+        root.right = sortedArrayToBST(rightArray)
+
+        return root
+    }
+
+    data class TreeNode(
+        val `val`: Int
+    ) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
